57ef2bef899adb84aa15fd49d017e4dd9a046ce7
haoyanwei <haoyanwei@elex-tech.com>
2022-05-12 20:22:52 +0800

    company

--- algorithm.js
+++ algorithm.js
@@ -1,8 +1,8 @@
 /*
 * @Author: yanwei
 * @Date:   2020-07-08 17:22:19
-* @Last Modified by:   yanwei
-* @Last Modified time: 2022-04-28 17:50:48
+* @Last Modified by:   haoyanwei
+* @Last Modified time: 2022-05-11 18:20:37
 //一些小算法
 */
 
@@ -18870,6 +18870,105 @@ i 能够被 perm[i] 整除
  * @return {number}
  */
 var countArrangement = function(n) {
+	//1回溯法 时间复杂度O(n方+n!) 空间复杂度O(n)
+	let used = new Array(n+1).fill(0)//是否访问过
+	let match = new Array(n+1)//匹配的内容
+	match[0] = []
+	for(let i = 1; i<=n; i++){
+		match[i] = []
+		for(let j = 1; j<=n; j++) {
+			if(i%j==0 || j%i==0) {
+				match[i].push(j)
+			}
+		}
+	}
+	let num = 0
+	let backTrack = function(idx){
+		if(idx == n+1) {
+			num++
+			return
+		}
+		for(let i = 0; i<match[idx].length; i++) {
+			let value = match[idx][i]
+			if(!used[value]) {
+				used[value] = true
+				backTrack(idx+1)
+				used[value] = false
+			}
+		}
+	}
+	backTrack(1)
+	return num
+};
+console.log('countArrangement:', countArrangement(3))
+
+/**
+528. 按权重随机选择
+给你一个 下标从 0 开始 的正整数数组 w ，其中 w[i] 代表第 i 个下标的权重。
+
+请你实现一个函数 pickIndex ，它可以 随机地 从范围 [0, w.length - 1] 内（含 0 和 w.length - 1）选出并返回一个下标。选取下标 i 的 概率 为 w[i] / sum(w) 。
+
+例如，对于 w = [1, 3]，挑选下标 0 的概率为 1 / (1 + 3) = 0.25 （即，25%），而选取下标 1 的概率为 3 / (1 + 3) = 0.75（即，75%）。
+ 
+
+示例 1：
+
+输入：
+["Solution","pickIndex"]
+[[[1]],[]]
+输出：
+[null,0]
+解释：
+Solution solution = new Solution([1]);
+solution.pickIndex(); // 返回 0，因为数组中只有一个元素，所以唯一的选择是返回下标 0。
+示例 2：
+
+输入：
+["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+[[[1,3]],[],[],[],[],[]]
+输出：
+[null,1,1,1,1,0]
+解释：
+Solution solution = new Solution([1, 3]);
+solution.pickIndex(); // 返回 1，返回下标 1，返回该下标概率为 3/4 。
+solution.pickIndex(); // 返回 1
+solution.pickIndex(); // 返回 1
+solution.pickIndex(); // 返回 1
+solution.pickIndex(); // 返回 0，返回下标 0，返回该下标概率为 1/4 。
+
+由于这是一个随机问题，允许多个答案，因此下列输出都可以被认为是正确的:
+[null,1,1,1,1,0]
+[null,1,1,1,1,1]
+[null,1,1,1,0,0]
+[null,1,1,1,0,1]
+[null,1,0,1,0,0]
+......
+诸若此类。
+ 
+
+提示：
+
+1 <= w.length <= 104
+1 <= w[i] <= 105
+pickIndex 将被调用不超过 104 次
+
+**/
+/**
+ * @param {number[]} w
+ */
+var Solution = function(w) {
 
 };
 
+/**
+ * @return {number}
+ */
+Solution.prototype.pickIndex = function() {
+
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(w)
+ * var param_1 = obj.pickIndex()
+ */
