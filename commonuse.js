f89ece6cffef831327b55ccbd0066b6084df4cda
haoyanwei <haoyanwei@elex-tech.com>
2022-12-08 20:38:16 +0800

    hhh

--- commonuse.js
+++ commonuse.js
@@ -2,7 +2,7 @@
 * @Author: haoyanwei
 * @Date:   2022-12-04 15:48:22
 * @Last Modified by:   haoyanwei
-* @Last Modified time: 2022-12-07 17:56:57
+* @Last Modified time: 2022-12-08 16:52:44
 * 常用的50道算法题
 */
 
@@ -594,4 +594,87 @@ console.log('generateParenthesis:', generateParenthesis(3))
 /**
 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
 https://leetcode-cn.com/problems/valid-parentheses/
+
+时间O(n)
+空间O(n)
 **/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+	let dic = {')':'(', '}':'{', ']':'['}
+	let stack = []
+	for(let i = 0; i<s.length; i++){
+		let item = dic[s[i]]
+		if (item && stack.pop()!=item) {
+			return false
+		}
+		if(!item){
+			stack.push(s[i])
+		}
+	}
+	return stack.length == 0
+};
+var s = "(()[])"
+console.log('isValid:', isValid(s))
+
+/**
+仅使用两个栈实现先入先出队列
+https://leetcode-cn.com/problems/implement-queue-using-stacks/
+**/
+var MyQueue = function() {
+
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+ /**
+ 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+
+实现 MyQueue 类：
+
+void push(int x) 将元素 x 推到队列的末尾
+int pop() 从队列的开头移除并返回元素
+int peek() 返回队列开头的元素
+boolean empty() 如果队列为空，返回 true ；否则，返回 false
+说明：
+
+你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ **/
